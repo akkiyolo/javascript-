af1861c7ae096dc1e82e0ad474da572cd0bbdaa0
akkiyolo <akshatshukla069@gmail.com>
2025-05-05 05:04:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 4411
+let accountEmail = "akshat@google.com"
+var accountPassword = "12345"
+accountCity = "lucknow"
+let accountstate;
+
+// accountId=2 not allowed 
+
+accountEmail="akki@google.com"
+accountPassword="21212121"
+accountCity="delhi"
+
+
+/*
+prefer not to use var 
+because of the issue of functional scope and block scope
+*/
+
+
+// console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello"); we are using node not browser
+
+console.log(+
+    +3                     // code readability should be high
+);
+
+
+let name="akshat"
+let age=20
+
+let isLoggedIn = false;
+
+let state = null
+
+// number => 2^52
+// bigint 
+// string => ""
+// boolean => either true or false
+// null (standalone value)
+// undefined (you did not define the value though you have declared a variable)
+// symbol => unique
+// objects
+
+console.log(typeof(age));
+console.log(typeof(null))
+console.log(typeof(undefined))
