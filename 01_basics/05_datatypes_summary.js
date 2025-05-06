9fa00aef235b982cad66abb3c0406217cf170f7f
akkiyolo <akshatshukla069@gmail.com>
2025-05-06 05:22:18 +0000

    summary of datatypes

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,22 @@
+// Primitive datatypes 
+
+// 7 datatypes : symbol , string , bigInt , Number , Boolean , Undefined , null
+
+// Reference (Non Primitive)
+
+// Arrays , objects , functions
+
+const heroes = ['nagraj','shaktiman','doga']
+ 
+let myObj={
+    name:"akki",
+    age:21
+
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof myFunction)
+
