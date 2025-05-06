eccb55b888f2ff5f30d72e558a7d022bf1cc0bdf
akkiyolo <akshatshukla069@gmail.com>
2025-05-06 04:57:12 +0000

    datatype conversion

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -1,12 +1,12 @@
 let score = "33abc"; // it will print not a number (NaN)
 
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof(valueInNumber));
+//console.log(typeof(valueInNumber));
 
-console.log(valueInNumber)
+//console.log(valueInNumber)
 
 
 // "33" => 33 using explicit conversion
@@ -18,7 +18,7 @@ let LoggedIn = "akki"
 
 let isloggedIn = Boolean(LoggedIn)
 
-console.log(isloggedIn);
+//console.log(isloggedIn);
 
 // true/false => 1/0
 // "" => false
@@ -27,4 +27,38 @@ console.log(isloggedIn);
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+/* ****operations*** */
+
+let value=3
+let negValue=-(value)
+// console.log(negValue);
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)*/
+
+let name ="hello"
+let name1 =" akki"
+// console.log(name + name1)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2) // if string is first then everything will be treated as string
+// console.log(1+2+"2") // if numbers are first then add those numbers and then merge with the string
+
+//console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=3+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+
